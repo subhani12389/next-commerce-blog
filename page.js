ee45cbaf61c9bfbf60125288f24f2205a8b00571
subhani12389 <sk5826314@gmail.com>
2025-12-15 14:45:59 +0530

    Add home page

---
+++ page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontSize: "24px" }}>
+      <h1>Next Commerce Blog</h1>
+      <p>Deployment successful 🚀</p>
+    </main>
+  );
+}
