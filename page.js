6692995f431ce19e3627572f284bd2a11b7057fc
subhani12389 <sk5826314@gmail.com>
2025-12-15 14:48:31 +0530

    Fix duplicate home page

--- page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "40px", fontSize: "24px" }}>
-      <h1>Next Commerce Blog</h1>
-      <p>Deployment successful 🚀</p>
-    </main>
-  );
-}
